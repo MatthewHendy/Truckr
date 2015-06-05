8302b571bc18875967c86c04cdd41e81ad0e1df5
MatthewHendy <matthewjames@utexas.edu>
2015-06-05 11:08:07 -0500

    Initial Commit

---
+++ Truckr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Truckr
+//
+//  Created by Matthew Hendrickson on 6/5/15.
+//  Copyright (c) 2015 Matthew Hendrickson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
